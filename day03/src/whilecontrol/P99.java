1c99e302aafcb802cbbd3cc737fcbd5ed4ea5c5d
eejeong <wdwaaa7325@gmail.com>
2023-02-27 10:13:34 +0900

    0227

--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -1,21 +1,18 @@
 package whilecontrol;
 
-import javax.imageio.event.IIOReadWarningListener;
-
 public class P99 {
 
 	public static void main(String[] args) {
-		int b = 20;
-		int aa = 100;
-		int a = 20;
-		int bb = 200;
-		int c = 20;
-		int cc = 100;
+		myfor: // 전체 for문 의미
 		for(int i=2;i<10;i++) {
 			System.out.printf("%d 단 \n",i);
 			for(int j=1;j<10;j++) {
-				System.out.printf("%d * %d = %d \n",i,j,(i*j));
+				System.out.printf("%d * %d = %d \t",i,j,(i*j));
+				if((i*j)==21) {
+					break myfor;
+				}
 			}
+			System.out.println("");
 		}
 	}
 
