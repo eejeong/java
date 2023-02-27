2d35386383b2d84eb4eed232342595c36b74238a
eejeong <wdwaaa7325@gmail.com>
2023-02-27 09:35:05 +0900

    add b

--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -1,9 +1,11 @@
 package whilecontrol;
 
+import javax.imageio.event.IIOReadWarningListener;
+
 public class P99 {
 
 	public static void main(String[] args) {
-		
+		int b = 20;
 		int bb = 200;
 		for(int i=2;i<10;i++) {
 			System.out.printf("%d 단 \n",i);
