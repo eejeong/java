72ea1db68bed3882bc53ec1a9f1ff1dcb81f33e8
eejeong <wdwaaa7325@gmail.com>
2023-03-07 17:19:18 +0900

    0307

---
+++ day09/src/bank/ShortfallException.java
@@ -0,0 +1,8 @@
+package bank;
+
+public class ShortfallException extends Exception {
+	public ShortfallException() {
+		
+	}
+
+}
---
+++ day09/src/bank/negativeNumException.java
@@ -0,0 +1,11 @@
+package bank;
+
+public class negativeNumException extends Exception {
+	public negativeNumException () {
+		
+	}
+	public negativeNumException(String msgcode) {
+		super(msgcode);
+	}
+
+}
