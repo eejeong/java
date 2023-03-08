55dfffac1b0c9db49f7f5d78223c22d12d34af0c
eejeong <wdwaaa7325@gmail.com>
2023-03-08 11:05:29 +0900

    0308 class

---
+++ day10/src/com/kbstar/dao/ItemDAO.java
@@ -0,0 +1,32 @@
+package com.kbstar.dao;
+
+import com.kbstar.dto.ItemDTO;
+import com.kbstar.frame.DAO;
+
+public class ItemDAO implements DAO<Integer, ItemDTO> { 
+// int는 key 값으로 선언 불가. Integer로 사용
+	
+	@Override
+	public void insert(ItemDTO v) {
+		connect();
+		System.out.println(v);
+		System.out.println("Inserted...");
+		close();
+	}
+
+	// int 값을 k값으로 치환하여 출력
+	@Override
+	public void delete(Integer k) {
+		connect();
+		System.out.println(k);
+		System.out.println("Deleted...");	
+		close();
+	}
+
+	@Override
+	public void update(ItemDTO v) {
+		System.out.println(v);
+		System.out.println("Updated...");
+	}
+
+}
